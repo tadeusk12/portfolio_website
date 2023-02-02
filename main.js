f94abe8a3554d0db7566a12619b96bfdb6e83118
tadeusk12 <78775956+tadeusk12@users.noreply.github.com>
2023-02-01 17:16:43 -0800

    added data vis

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ var ready = (callback) => {
 ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
+
+const toggleButton = document.querySelector("#toggle-button");
+const toggleText = document.querySelector("#toggle-text");
+const toggleLogo = document.querySelector("#toggle-logo");
+
+toggleButton.addEventListener("click", function() {
+  if (toggleText.style.display === "none") {
+    toggleText.style.display = "block";
+    toggleLogo.setAttribute("src", "your-logo-up.png");
+  } else {
+    toggleText.style.display = "none";
+    toggleLogo.setAttribute("src", "your-logo.png");
+  }
+});
